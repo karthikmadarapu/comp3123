1b98cd171d8e1cc329e8517968b3b2cdcf2986c6
Karthik Madarapu <karthikmadarapu21@gmail.com>
2026-09-16 11:49:43 -0400

    filter , map , reduce , forEach methods

---
+++ labs/lab2/lab2_array_methods.js
@@ -0,0 +1,18 @@
+/*
+purpose:
+filter()
+reduce()
+map()
+forEach()
+search()
+*/
+
+
+const courses = ["COMP3123", "C0MP 3074", "COMP 3122", "GSCCC"];
+
+// 
+ const filtering_courses = courses.filter((course) =>   course[0] === "C" );
+
+console.log(filtering_courses);
+
+
